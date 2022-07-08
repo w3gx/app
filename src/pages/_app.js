c042e3b3863aeabe217d95c77bf9addac94878ce
elcharitas <jonathanirhodia@gmail.com>
2022-07-08 12:02:02 +0100

    chore: add material pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,40 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import { CacheProvider } from "@emotion/react";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { createEmotionCache } from "../utils/create-emotion-cache";
+import { theme } from "../theme";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const clientSideEmotionCache = createEmotionCache();
 
-export default MyApp
+const App = (props) => {
+    const {
+        Component,
+        emotionCache = clientSideEmotionCache,
+        pageProps,
+    } = props;
+
+    const getLayout = Component.getLayout ?? ((page) => page);
+
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <title>W3G</title>
+                <meta
+                    name="viewport"
+                    content="initial-scale=1, width=device-width"
+                />
+            </Head>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    {getLayout(<Component {...pageProps} />)}
+                </ThemeProvider>
+            </LocalizationProvider>
+        </CacheProvider>
+    );
+};
+
+export default App;
