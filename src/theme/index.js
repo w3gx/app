0756fd73743293f0c6d200341695b1c5e459f27e
elcharitas <jonathanirhodia@gmail.com>
2022-07-09 11:38:49 +0100

    fix: pass theme options to context

--- src/theme/index.js
+++ src/theme/index.js
@@ -4,18 +4,21 @@ import { ThemeProvider } from "@mui/material/styles";
 import { useStore } from "src/hooks";
 import { lightTheme, darkTheme } from "./themes";
 
-const ThemeContext = createContext({ theme: "light", setTheme: () => {} });
+const ThemeContext = createContext({
+    theme: "light",
+    setTheme: () => {},
+    options: {},
+});
 
 export const useTheme = () => useContext(ThemeContext);
 
 export const W3GThemeProvider = ({ children }) => {
     const [theme, setTheme] = useStore("w3g-theme");
+    const options = theme === "light" ? lightTheme : darkTheme;
 
     return (
-        <ThemeContext.Provider value={{ theme, setTheme }}>
-            <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-                {children}
-            </ThemeProvider>
+        <ThemeContext.Provider value={{ theme, setTheme, options }}>
+            <ThemeProvider theme={options}>{children}</ThemeProvider>
         </ThemeContext.Provider>
     );
 };
