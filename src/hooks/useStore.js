c826840fcd3caa23332a98da0bb9c2fdaae0db10
elcharitas <jonathanirhodia@gmail.com>
2022-07-10 13:58:29 +0100

    chore: add useStore docs

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,6 +1,13 @@
 import { useEffect, useState } from "react";
 import { store } from "src/utils";
 
+/**
+ * Optimistically stores a value in localStorage
+ *
+ * @param {string} key
+ * @param {any} initialValue
+ * @returns
+ */
 const useStore = (key, initialValue) => {
     const storedValue = store(key) || initialValue;
     const [state, setState] = useState(storedValue);
